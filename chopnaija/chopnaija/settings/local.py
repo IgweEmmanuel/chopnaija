1fd97b69e4eb8c70dec3644bb4d3240e6c775c47
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-10-10 21:34:36 +0100

    create .env file for setting up keys

--- chopnaija/chopnaija/settings/local.py
+++ chopnaija/chopnaija/settings/local.py
@@ -1,8 +1,14 @@
 from .base import *
+from dotenv import load_dotenv
 import os
-from getpass import getpass
-SECRET_KEY = getpass('Enter key')#'b#7$%2_v(&narw_0-08d&okq^$_+yry0#+#_g3e4n1+tvu2!%y'
-os.environ['SECRET_KEY'] = SECRET_KEY
+
+load_dotenv()
+# from getpass import getpass
+# SECRET_KEY = getpass('Enter key')#'b#7$%2_v(&narw_0-08d&okq^$_+yry0#+#_g3e4n1+tvu2!%y'
+# os.environ['SECRET_KEY'] = SECRET_KEY
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
