6b58c4ad85f8de1c1c149d002a3333ce8efd0272
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-10-12 22:45:22 +0100

    create views to handle business logic and send data back to users after serialization

--- chopnaija/chopnaija/product/views.py
+++ chopnaija/chopnaija/product/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category
+from .serializers import CategorySerializer
+from rest_framework.response import Response
 
-# Create your views here.
+class CategoryView(viewsets.ViewSet):
+    """Category view
+
+    Args:
+        viewsets: views for client
+    """
+    queryset = Category.objects.all()
+    
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
