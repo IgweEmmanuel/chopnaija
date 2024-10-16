49a42c503f6d7858560dd1d57ec014a25ac686fe
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-10-16 15:58:45 +0100

    install drf-spectacular and generate schema.yml file

--- chopnaija/chopnaija/product/views.py
+++ chopnaija/chopnaija/product/views.py
@@ -3,15 +3,17 @@ from rest_framework import viewsets
 from .models import Category
 from .serializers import CategorySerializer
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 
 class CategoryView(viewsets.ViewSet):
     """Category view
 
     Args:
-        viewsets: views for client
+        viewsets: views for client access to database data
     """
     queryset = Category.objects.all()
     
+    @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
         return Response(serializer.data)
