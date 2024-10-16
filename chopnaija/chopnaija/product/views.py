9586ae76490444d0bf44b327da7b09025326e0a4
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-10-16 16:38:55 +0100

    add brand and product serializers to views and registered them in urls

--- chopnaija/chopnaija/product/views.py
+++ chopnaija/chopnaija/product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Brand, Product
+from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
 
@@ -16,4 +16,32 @@ class CategoryView(viewsets.ViewSet):
     @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class BrandView(viewsets.ViewSet):
+    """Category view
+
+    Args:
+        viewsets: views for client access to database data
+    """
+    queryset = Brand.objects.all()
+    
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+    
+    
+class ProductView(viewsets.ViewSet):
+    """Product view
+
+    Args:
+        viewsets: views for client access to database data
+    """
+    queryset = Product.objects.all()
+    
+    @extend_schema(responses=ProductSerializer)
+    def list(self, request):
+        serializer = ProductSerializer(self.queryset, many=True)
         return Response(serializer.data)
