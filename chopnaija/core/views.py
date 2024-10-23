d9c13cf573ca9732fac079442b809e02d0de4f47
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-10-23 01:01:10 +0100

    get users from database and create new users

--- chopnaija/core/views.py
+++ chopnaija/core/views.py
@@ -8,12 +8,28 @@ from .models import CustomUser
 
 @api_view(['GET'])
 def get_users(request):
+    """
+    Get all users
+    Args:
+        request (dict): request object
+
+    Returns:
+        dict : users data
+    """
     users = CustomUser.objects.all()
     serializer = CustomUserSerializer(users, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
 def create_user(request):
+    """
+    Create a new user
+    Args:
+        request (dict): request object
+
+    Returns:
+        dict : user data
+    """
     serializer = CustomUserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
